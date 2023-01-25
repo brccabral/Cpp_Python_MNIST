770a05ad715eb0e902b4125d071c85c7cd738a22
Bruno Cabral <cabral.brc@gmail.com>
2023-01-25 11:12:50 -0800

    back_prop

--- converter_mnist.py
+++ converter_mnist.py
@@ -124,6 +124,38 @@ def forward_prop(
     return Z1, A1, Z2, A2
 
 
+def one_hot(Y: np.ndarray) -> np.ndarray:
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))  # create matrix with correct size
+    # np.arange(Y.size) - will return a list from 0 to Y.size
+    # Y - will contain the column index to set the value of 1
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    return one_hot_Y.T  # transpose
+
+
+def deriv_ReLU(Z: np.ndarray) -> np.ndarray:
+    return Z > 0
+
+
+def back_prop(
+    Z1: np.ndarray,
+    A1: np.ndarray,
+    Z2: np.ndarray,
+    A2: np.ndarray,
+    W2: np.ndarray,
+    X: np.ndarray,
+    Y: np.ndarray,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    m = Y.size
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1: np.ndarray = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
+
+
 def main(argc: int, argv: list[str]):
 
     np.random.seed(int(time.time()))
@@ -157,6 +189,7 @@ def main(argc: int, argv: list[str]):
 
     for generation in range(num_generations):
         Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W2, X, Y_train)
 
 
 if __name__ == "__main__":
