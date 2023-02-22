30fed33944c6d77286d6675d7109cdcdf16b7f31
Bruno Cabral <cabral.brc@gmail.com>
2023-02-22 13:04:32 -0800

    cpp use custom model in batches

--- mnist_batches.cpp
+++ mnist_batches.cpp
@@ -1,55 +1,7 @@
 #include <SimpleIni/SimpleIni.h>
 #include <torch/torch.h>
 #include <iostream>
-
-// Define a new Module.
-struct Net : torch::nn::Module
-{
-    Net()
-    {
-        // Construct and register two Linear submodules.
-        fc1 = register_module("fc1", torch::nn::Linear(784, 64));
-        fc2 = register_module("fc2", torch::nn::Linear(64, 32));
-        fc3 = register_module("fc3", torch::nn::Linear(32, 10));
-    }
-
-    // Implement the Net's algorithm.
-    torch::Tensor forward(torch::Tensor x)
-    {
-        // Use one of many tensor manipulation functions.
-        x = torch::relu(fc1->forward(x.reshape({x.size(0), 784})));
-        x = torch::dropout(x, /*p=*/0.5, /*train=*/is_training());
-        x = torch::relu(fc2->forward(x));
-        x = torch::log_softmax(fc3->forward(x), /*dim=*/1);
-        return x;
-    }
-
-    // Use one of many "standard library" modules.
-    torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
-};
-
-// Define a new Module.
-struct Net2 : torch::nn::Module
-{
-    Net2()
-    {
-        // Construct and register two Linear submodules.
-        fc1 = register_module("fc1", torch::nn::Linear(784, 10));
-        fc3 = register_module("fc3", torch::nn::Linear(10, 10));
-    }
-
-    // Implement the Net's algorithm.
-    torch::Tensor forward(torch::Tensor x)
-    {
-        // Use one of many tensor manipulation functions.
-        x = torch::relu(fc1->forward(x.reshape({x.size(0), 784})));
-        x = torch::log_softmax(fc3->forward(x), /*dim=*/1);
-        return x;
-    }
-
-    // Use one of many "standard library" modules.
-    torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
-};
+#include <TorchNet/torchnet.hpp>
 
 int main()
 {
@@ -69,14 +21,7 @@ int main()
     int num_epochs = ini.GetLongValue("TORCH", "EPOCHS", 10);
     std::string save_model = ini.GetValue("TORCH", "SAVE_CPP", "net_cpp.pt");
     float alpha = ini.GetDoubleValue("MNIST", "ALPHA", 0.1);
-
-    // Create a new Net.
-    // auto net = std::make_shared<Net>();
-    // torch::optim::SGD optimizer(net->parameters(), /*lr=*/0.01);
-    auto net = std::make_shared<Net2>();
-    // Instantiate an SGD optimization algorithm to update our Net's parameters.
-    torch::optim::SGD optimizer(net->parameters(), /*lr=*/alpha);
-    torch::nn::NLLLoss loss_fn;
+    int hidden_layer_size = ini.GetLongValue("MNIST", "HIDDEN_LAYER_SIZE", 10);
 
     // Create a multi-threaded data loader for the MNIST dataset.
     auto train_dataset = torch::data::datasets::MNIST(base_dir);
@@ -97,6 +42,31 @@ int main()
         torch::data::datasets::MNIST(base_dir).map(torch::data::transforms::Stack<>()),
         batch_size);
 
+    // Create a new Net.
+    // auto net = std::make_shared<Net>();
+    // torch::optim::SGD optimizer(net->parameters(), /*lr=*/0.01);
+    auto net = std::make_shared<Net>(num_features, hidden_layer_size, categories);
+    std::cout << *net << std::endl;
+
+    c10::DeviceType device;
+
+    if (torch::cuda::is_available())
+    {
+        std::cout << "Using cuda" << std::endl;
+        // Move model and inputs to GPU
+        device = torch::kCUDA;
+    }
+    else
+    {
+        device = torch::kCPU;
+    }
+    net->to(device);
+
+    // Instantiate an SGD optimization algorithm to update our Net's parameters.
+    torch::optim::SGD optimizer(net->parameters(), /*lr=*/alpha);
+    torch::nn::NLLLoss loss_fn;
+    torch::nn::Flatten flatten;
+
     std::tuple<torch::Tensor, torch::Tensor> tm;
     torch::Tensor prediction, values, indices, correct_bool;
     int correct_prediction;
@@ -109,12 +79,16 @@ int main()
         // Iterate the data loader to yield batches from the dataset.
         for (auto &batch : *train_dataloader)
         {
+            torch::Tensor X = batch.data.to(device);
+            torch::Tensor y = batch.target.to(device);
+            X = flatten(X);
+
             // Reset gradients.
             optimizer.zero_grad();
             // Execute the model on the input data.
-            prediction = net->forward(batch.data);
+            prediction = net->forward(X);
             // Compute a loss value to judge the prediction of our model.
-            torch::Tensor loss = loss_fn(prediction, batch.target); // (64x10, 64x1)
+            torch::Tensor loss = loss_fn(prediction, y); // (64x10, 64x1)
             // Compute gradients of the loss w.r.t. the parameters of our model.
             loss.backward();
             // Update the parameters based on the calculated gradients.
@@ -137,7 +111,7 @@ int main()
         }
     }
 
-    auto net_loaded = std::make_shared<Net2>();
+    auto net_loaded = std::make_shared<Net>(num_features, hidden_layer_size, categories);
     torch::load(net_loaded, save_model);
     net_loaded->eval();
 
@@ -170,12 +144,16 @@ int main()
     correct_prediction = 0;
     for (auto &batch_test : *test_dataloader)
     {
-        prediction = net_loaded->forward(batch_test.data);
+        torch::Tensor X = batch_test.data.to(device);
+        torch::Tensor y = batch_test.target.to(device);
+        X = flatten(X);
+
+        prediction = net_loaded->forward(X);
 
         tm = torch::max(prediction, 1);
         std::tie(values, indices) = tm;
 
-        correct_bool = batch_test.target == indices;
+        correct_bool = y == indices;
         correct_prediction += correct_bool.sum().item<int>();
     }
     acc = 1.0f * correct_prediction / size;
